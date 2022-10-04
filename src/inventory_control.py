f665dcef6910ce41e82382c69ed8111cf6664a58
Rodrigo Lima <limarodrigoo@outlook.com>
2022-10-04 14:56:54 -0300

    feat: implement add order and quantities to buy

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,29 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self._orders = list()
+        self._buy_list = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+        self._out_of_stock = set()
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._orders.append((customer, order, day))
+        ingredient_out_of_stock = set(self.INGREDIENTS).intersection(self._out_of_stock)
+        if len(ingredient_out_of_stock) == 0:
+            for ingridient in self.INGREDIENTS[order]:
+                self._buy_list[ingridient] += 1
+                current_stock = self.MINIMUM_INVENTORY[ingridient] - self._buy_list[ingridient]
+                if current_stock <= 0:
+                    self._out_of_stock.add(ingridient)
+            return False
+
 
     def get_quantities_to_buy(self):
-        pass
+        return self._buy_list
