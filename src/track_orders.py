d1a450ce5e472cbaa41408618854c41861601def
Rodrigo Lima <limarodrigoo@outlook.com>
2022-10-04 14:06:45 -0300

    feat: implement TrackOrders class

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,40 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self._orders = list()
+
     def __len__(self):
-        pass
+        return len(self._orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        return self._orders.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders = Counter(
+            order for name, order, _ in self._orders if name == customer
+        )
+        return max(orders, key=orders.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_orders = set(order for _, order, _ in self._orders)
+        client_orders = set(
+            order for name, order, _ in self._orders if name == customer
+        )
+        return all_orders - client_orders
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_days = set(day for _, _, day in self._orders)
+        client_attend = set(
+            day for name, _, day in self._orders if name == customer
+        )
+        return all_days - client_attend
 
     def get_busiest_day(self):
-        pass
+        days = Counter(day for _, _, day in self._orders)
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
-        pass
+        days = Counter(day for _, _, day in self._orders)
+        return min(days, key=days.get)
